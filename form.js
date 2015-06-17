c46a344e2266e1d14501a4a3627ee85d30e414fb
tjmasterson <taylor.j.masterson@gmail.com>
2015-06-17 14:11:50 -0500

    added call to google api to verify success to be included in authentication

--- form.js
+++ form.js
@@ -8,6 +8,7 @@ $(function(){
 
   // my key as an example of googles public cite key
   // var myKey = '6LcufQgTAAAAAJ4IAJyTUfY8BAr2yCWa7ul7wpnh'
+  // var mySecretKey = '6LcufQgTAAAAAJ4IAJyTUfY8BAr2yCWa7ul7wpnh'
 
   // method to call when user has tried to login 3 times or more
   var onloadCallback = function() {
@@ -19,7 +20,9 @@ $(function(){
 
   // callback funciton when user has answered reCaptcha correctly
   var verifyCallback = function(response) {
-    console.log('execute logic on the backend to store user in session etc...')
+    if (grecaptcha.getResponse('g-recaptch-widget-1').success === 'true') {
+      // execute logic on the backend authenticate user and store user info in session etc if successful
+    };
   };
 
   // listen for user trying to login, implement reCaptcha after 3 failed login attempts
